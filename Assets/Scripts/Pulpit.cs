d727bc4204d9b72161ed7a22b333ff5fdc863b61
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2024-08-25 21:32:00 +0530

    Level 1 Complete: Implement Pulpit
    I implemented the pulpit mechanism and completed level 1 of the assignment

---
+++ Assets/Scripts/Pulpit.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pulpit : MonoBehaviour
+{
+   private float timeToLive;
+    private float minTimeToLive;
+    private float maxTimeToLive;
+    
+    private float timeToNextPulpit;
+
+    public float currentTimetoLive;
+    public bool pulpitSpawnFlag = false;
+
+    public TMP_Text timerText;
+
+
+
+    private void Start()
+    {
+        // Get the min and max time a pulpit can exist from the GameDataManager
+        minTimeToLive = GameDataManager.instance.pulpitData.min_pulpit_destroy_time;
+        maxTimeToLive = GameDataManager.instance.pulpitData.min_pulpit_destroy_time;
+        timeToLive = Random.Range(minTimeToLive, maxTimeToLive);
+
+        timeToNextPulpit = GameDataManager.instance.pulpitData.pulpit_spawn_time;
+
+        currentTimetoLive = timeToLive;
+
+        StartCoroutine(DestroyPulpit());
+    }
+
+    private void Update()
+    {
+        currentTimetoLive -= Time.deltaTime;
+        timerText.text = currentTimetoLive.ToString("F2");
+        if (!pulpitSpawnFlag && PulpitController.instance.pulpitCount<2 && currentTimetoLive <= timeToNextPulpit)
+        {
+            PulpitController.instance.SpawnPulpit(gameObject.transform.position);
+            pulpitSpawnFlag = true;
+        }
+    }
+
+    private IEnumerator DestroyPulpit()
+    {
+        yield return new WaitForSeconds(timeToLive);
+
+        PulpitController.instance.pulpitCount--;
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/PulpitController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulpitController : MonoBehaviour
+{
+    // Pulpits are platforms the player can walk on
+    // Pulpits don't last long and disappear within seconds
+    // Each Pulpit has a timer. When it runs out, the Pulpit is destroyed, and the player falls
+    // Only two Pulpits can exist simultaneously. 
+    // A new Pulpit appears when the timer of the previous one reaches a certain time (x seconds). 
+    // Pulpits last for a random time between y and z seconds. 
+    // They appear adjacent to the previous one, but not in the same position.
+
+    public static PulpitController instance;
+
+    public GameObject pulpitPrefab;
+
+    public GameObject currentPulpit;
+    public GameObject nextPulpit;
+
+    public float pulpitDistance = 2.0f;
+    public int pulpitCount = 1;
+
+    private int previousRandomDirection;
+
+    private void Awake()
+    {
+        // Singleton class
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void SpawnPulpit(Vector3 originPos)
+    {
+        // Select a random direction for the next Pulpit, from North, South, East, West
+        Vector3 direction = Vector3.zero;
+        int randomDirection = -1;
+        // Make sure the next Pulpit is not in the same position as the previous one
+        while (randomDirection == -1 || randomDirection == previousRandomDirection)
+        {
+            randomDirection = Random.Range(0, 4);
+        }
+        switch (randomDirection)
+        {
+            case 0:
+                direction = Vector3.forward;
+                previousRandomDirection = 1;
+                break;
+            case 1:
+                direction = Vector3.back;
+                previousRandomDirection = 0;
+                break;
+            case 2:
+                direction = Vector3.right;
+                previousRandomDirection = 3;
+                break;
+            case 3:
+                direction = Vector3.left;
+                previousRandomDirection = 2;
+                break;
+        }
+
+        
+
+        // Calculate the position of the next Pulpit
+        Vector3 nextPulpitPos = originPos + direction * pulpitDistance;
+
+        Instantiate(pulpitPrefab, nextPulpitPos, Quaternion.identity);
+
+        pulpitCount++;
+
+    }
+
+    // Coroutine to spawn Pulpit after a certain time
+    public IEnumerator SpawnPulpitAfterTime(Vector3 position)
+    {
+        yield return new WaitForSeconds(GameDataManager.instance.pulpitData.pulpit_spawn_time);
+        SpawnPulpit(position);
+    }
+
+}

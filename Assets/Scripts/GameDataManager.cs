20640d082864b942d8787096d3ac4f8ecc45b740
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2024-08-25 18:52:05 +0530

    Create GameDataManager
    Created the GameDataManager script which reads the JSON files and stores it in a singleton

---
+++ Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerData
+{
+    public float speed;
+}
+
+[System.Serializable]
+public struct PulpitData
+{
+    public float min_pulpit_destroy_time;
+    public float max_pulpit_destroy_time;
+    public float pulpit_spawn_time;
+}
+
+// Struct containing PlayerData and PulpitData
+public struct DoofusData
+{
+    public PlayerData player_data;
+    public PulpitData pulpit_data;
+}
+
+[DefaultExecutionOrder(-100)]
+public class GameDataManager : MonoBehaviour
+{
+    // Singleton class that contains player data and pulpit data
+    // This script is attached to the GameManager object
+
+    public static GameDataManager instance;
+
+    public DoofusData doofusData;
+    public PlayerData playerData;
+    public PulpitData pulpitData;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Read Doofusdata from JSON file
+        string json = Resources.Load<TextAsset>("doofus_diary").ToString();
+        Debug.Log(json);
+        doofusData = JsonUtility.FromJson<DoofusData>(json);
+        playerData = doofusData.player_data;
+        pulpitData = doofusData.pulpit_data;
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController3D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour
+{
+    // A simple player controller that allows for movement on the X and Z axis
+    // This script is attached to the player object
+
+    private float speed = 5.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}

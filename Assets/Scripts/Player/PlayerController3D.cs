0fdcb83367c68e1cbcd0934d03178402fb2e045b
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2024-08-26 19:46:49 +0530

    Some changes to make the build

--- Assets/Scripts/Player/PlayerController3D.cs
+++ Assets/Scripts/Player/PlayerController3D.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
